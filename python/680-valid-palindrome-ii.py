608ea589e608c087f5ca1dfb4f1b8753997743c0
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2020-09-03 02:29:38 -0700

    Update 680-valid-palindrome-ii.py

--- python/680-valid-palindrome-ii.py
+++ python/680-valid-palindrome-ii.py
@@ -3,7 +3,10 @@ Complexity Analysis
 
     Time Complexity: O(N) where N is the length of the string. Each of two checks of whether some substring is a palindrome is O(N).
     Space Complexity: O(1) additional complexity. Only pointers were stored in memory.
-    
+
+Note: 
+1. isPalindrome checks whether a given string s is palindrome
+2. Key idea is: if s[i] == s[j] then we may take i++; j--. Otherwise, the palindrome must be either s[i+1], s[i+2], ..., s[j] or s[i], s[i+1], ..., s[j-1], and we should check both cases.
 """
 
 class Solution:
