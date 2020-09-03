90078bcaf528a5586dbe91a9cc6ee0b2bc5ecffe
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2020-09-03 02:14:28 -0700

    Create 020-valid-parentheses.py

---
+++ python/020-valid-parentheses.py
@@ -0,0 +1,29 @@
+"""
+Complexity analysis:
+
+    Time complexity : O(n) because we simply traverse the given string one character at a time and push and pop operations on a stack take O(1) time.
+    Space complexity : O(n) as we push all opening brackets onto the stack and in the worst case, we will end up pushing all the brackets onto the stack. e.g. ((((((((((.
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # corner case
+        if not s:
+            return True
+        
+        mapping = {')':'(', ']':'[', '}':'{'}
+        left_chars = set(['(','[','{'])
+        right_chars = set([')',']','}'])
+        
+        stack = []
+        for char in s:
+            if char in left_chars:
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                pop_char = stack.pop()
+                if pop_char != mapping[char]:
+                    return False
+        
+        return not stack
