2d581c46a58a457d7644edc566aca6cd063a8541
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2020-09-03 02:18:32 -0700

    Update 020-valid-parentheses.py

--- python/020-valid-parentheses.py
+++ python/020-valid-parentheses.py
@@ -3,6 +3,7 @@ Complexity analysis:
 
     Time complexity : O(n) because we simply traverse the given string one character at a time and push and pop operations on a stack take O(1) time.
     Space complexity : O(n) as we push all opening brackets onto the stack and in the worst case, we will end up pushing all the brackets onto the stack. e.g. ((((((((((.
+    
 """
 
 class Solution:
