0eb711245f477dc521dc7d54ae904e66aef1d512
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2020-09-12 03:17:29 -0700

    Create 647-palindromic-substrings.py

---
+++ python/647-palindromic-substrings.py
@@ -0,0 +1,23 @@
+"""
+Time: O(n^2)
+Space: O(1)
+"""
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        for i in range(len(s)):
+            res += self.expandAroundCenter(s, i, i)
+            res += self.expandAroundCenter(s, i, i + 1)
+        return res
+    
+    def expandAroundCenter(self, s, l, r):
+        if l < 0 or r >= len(s):
+            return 0
+        cnt = 0
+        while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
+            cnt += 1
+            l -= 1
+            r += 1
+        return cnt
+            
