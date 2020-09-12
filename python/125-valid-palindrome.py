bca9840d7a9428ca43d1db5f6ebc9adc97cfc043
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2020-09-12 03:14:43 -0700

    Create 125-valid-palindrome.py

---
+++ python/125-valid-palindrome.py
@@ -0,0 +1,19 @@
+"""
+Time: O(n)
+Space: O(1)
+"""
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalpha() and not s[i].isdigit():
+                i += 1
+            while i < j and not s[j].isalpha() and not s[j].isdigit():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            else:
+                i += 1
+                j -= 1
+        return True
