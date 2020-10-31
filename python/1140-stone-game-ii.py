33a828fbd0c4376cfab2bc1bf3e2e6f43b806473
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2020-10-31 12:00:11 -0700

    Update 1140-stone-game-ii.py

--- python/1140-stone-game-ii.py
+++ python/1140-stone-game-ii.py
@@ -1,6 +1,10 @@
 """
-Time: O(n^2), memo of size n * M filled once, where M is bounded by n, thus n^2
+Time: O(n^3), similiar as https://github.com/teslamyesla/leetcode/blob/master/python/0375-guess-number-higher-or-lower-ii.py
 Space: O(n^2), memo size n * M, where M is bounded by n, thus n^2
+
+关于dp时间复杂度：dp[(i,M)]自问题从小到达过一遍，至少是n^2复杂度，但n^2还是n^3主要区别在于更新dp[(i,j)]时是只基于某几个dp[(i-1, j)], dp[(i, j-1)]的子问题，还是基于
+dp[(i-1, j)], dp[(i-2, j)], ...等几乎之前所有的子问题。例如，https://github.com/teslamyesla/leetcode/blob/master/python/0486-predict-the-winner.py 这种两端取问题，
+只能取首尾，就是n^2；https://github.com/teslamyesla/leetcode/blob/master/python/0375-guess-number-higher-or-lower-ii.py 这种中间都能取的问题，就是n^3。
 """
 
 class Solution:
