591870dff8248d5ba35ed9290e1c41bd7b5c5e8f
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2020-11-09 18:45:02 -0800

    Update 1406-stone-game-iii.py

--- python/1406-stone-game-iii.py
+++ python/1406-stone-game-iii.py
@@ -1,13 +1,42 @@
 """ 
+Solution 1: Top Down DFS + memo
 Time: O(n)
 Space: O(n)
-"""
 
-"""
+Solution 2: Bottom Up DP
+Time: O(n)
+Space: O(n)
+
 Trick: dfs returns diff instead of absolute value of the first picker
 """
 
 class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        """
+        Solution 2: Bottom Up DP
+        """
+        n = len(stoneValue)
+        dp = [float('-inf')] * (n + 1)
+        dp[n] = 0
+        
+        for i in range(n-1, -1, -1):
+            taken = 0
+            for j in range(1, 4):
+                if i + j - 1 >= n:
+                    break
+                taken += stoneValue[i + j - 1]
+                dp[i] = max(dp[i], taken - dp[i+j])
+                
+        if dp[0] > 0:
+            return 'Alice'
+        elif dp[0] < 0:
+            return 'Bob'
+        else:
+            return 'Tie'
+        
+    """
+    Solution 1: Top Down DFS + memo
+    
     def stoneGameIII(self, stoneValue: List[int]) -> str:
         memo = {}
         res = self.dfs(stoneValue, 0, memo)
@@ -36,5 +65,6 @@ class Solution:
         
         memo[idx] = res
         return memo[idx]
+    """
             
             
