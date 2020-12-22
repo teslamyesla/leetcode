c6f235c15646567bdcc6a27cd25346091b53c12b
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2020-12-21 17:36:26 -0800

    Create lintcode-0433-number-of-islands.py

---
+++ python/lintcode-0433-number-of-islands.py
@@ -0,0 +1,36 @@
+
+
+
+class Solution:
+    """
+    @param grid: a boolean 2D matrix
+    @return: an integer
+    """
+    def numIslands(self, grid):
+        # write your code here
+        if not grid or not grid[0]:
+            return 0
+            
+        m, n = len(grid), len(grid[0])
+        count = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    self.dfs(grid, i, j)
+                    count += 1
+                    
+        return count
+        
+    def dfs(self, grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0: # 0 is sea or visited island
+            return
+        
+        grid[i][j] = 0 # visited, mark as 0
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i, j - 1)
+        self.dfs(grid, i, j + 1)
+        
+        return
+        
