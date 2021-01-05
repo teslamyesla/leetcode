1e8c054a26d06f915e2d200fee114f2e2c0c90a8
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2021-01-05 00:47:44 -0800

    Create lintcode-0453-flatten-binary-tree-to-linked-list.py

---
+++ python/lintcode-0453-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,42 @@
+"""
+Time: O(n)
+Space: O(h)
+"""
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    """
+    Solution: Divide and Conquer
+    
+    flatten_and_return_last_node函数 return 这个 tree 的最后一个点。
+    """
+    def flatten(self, root):
+        # write your code here
+        self.flatten_and_return_last_node(root)
+        return root
+        
+    def flatten_and_return_last_node(self, root):
+        if root is None:
+            return None
+            
+        left_last = self.flatten_and_return_last_node(root.left)
+        right_last = self.flatten_and_return_last_node(root.right)
+        
+        # connect
+        if left_last is not None:
+            left_last.right = root.right
+            root.left, root.right = None, root.left
+            
+        return right_last or left_last or root
+    
