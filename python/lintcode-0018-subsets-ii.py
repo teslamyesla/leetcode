ddefbba61540167c175797e4b2c9fd33c3fffe6b
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2021-01-19 13:39:20 -0800

    Update lintcode-0018-subsets-ii.py

--- python/lintcode-0018-subsets-ii.py
+++ python/lintcode-0018-subsets-ii.py
@@ -8,6 +8,12 @@ Reference:
 有重复数字，需要：
 1. sort
 2. 每个重复数字只第一个进loop的时候加一次，否则会12(1), 12(2)
+
+Solution 2里剪枝只能减not choose的原因是：
+1. [1,2,2] 想保留[1,2,2]两个2都choose的情况，所以choose不可剪枝
+2. not choose剪枝剪的是 [1]前一个2 choose了->[1,2] 后一个2 not choose的情况，通过比较path[-1] == nums[start]完成
+   能不能反过来剪前一个2 not choose, 后一个2 choose的情况呢？是不能的，因为没choose的时候path里没有记录，所以无法追溯
+   
 """
 class Solution:
     """
