e773576a985e16a9ebbd4f9bcf56186003d03842
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2021-01-20 13:17:55 -0800

    Update lintcode-0015-permutations.py

--- python/lintcode-0015-permutations.py
+++ python/lintcode-0015-permutations.py
@@ -5,6 +5,12 @@ Space: O(n) (ignore res)
 Reference: 
 https://leetcode.com/problems/combination-sum/discuss/16502/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
 https://www.youtube.com/watch?v=8t7bIHIr9JY
+
+Notes: (Similiar as Subsets, Combinations, with below difference)
+1. No "start" parameter, because for permutation, you can go back to starting element when it's not used yet
+2. When adding a new element to path, for loop through range(0, len(nums))[comment: permutation!] instead of (start, len(nums)) [comment: combination!] 
+3. Ending condition is len(path) == len(nums) instead of start == len(nums)
+
 """
 
 class Solution:
