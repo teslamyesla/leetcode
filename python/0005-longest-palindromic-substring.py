80c254d4b79971b18a20c56dc94588a3b50e1da3
teslamyesla <70689612+teslamyesla@users.noreply.github.com>
2021-01-29 12:49:36 -0800

    Update 0005-longest-palindromic-substring.py

--- python/0005-longest-palindromic-substring.py
+++ python/0005-longest-palindromic-substring.py
@@ -29,4 +29,41 @@ class Solution:
             l -= 1
             r += 1
         return l+1, r-1
+    
+    
+"""
+基于区间型动态规划的解法
+
+Time: O(n^2)
+Space: O(n^2)
+"""
+
+class Solution:
+    """
+    @param s: input string
+    @return: the longest palindromic substring
+    """
+    def longestPalindrome(self, s):
+        if not s:
+            return ""
+            
+        n = len(s)
+        is_palindrome = [[False] * n for _ in range(n)]
+        
+        for i in range(n):
+            is_palindrome[i][i] = True
+        for i in range(1, n):
+            is_palindrome[i][i - 1] = True
+            
+        longest, start, end = 1, 0, 0
+        for length in range(1, n):
+            for i in range(n - length):
+                j = i + length
+                is_palindrome[i][j] = s[i] == s[j] and is_palindrome[i + 1][j - 1]
+                if is_palindrome[i][j] and length + 1 > longest:
+                    longest = length + 1
+                    start, end = i, j
+                    
+        return s[start:end + 1]
+
             
